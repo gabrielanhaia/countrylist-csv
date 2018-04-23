df13a8c403cfd7c774dd6665b0aebc2ca278acf5
Gabriel Anhaia <anhaia.gabriel@gmail.com>
2018-04-23 09:51:34 -0300

    FEA: Base de testes do pacote.

---
+++ tests/CustomGenerator/CsvGeneratorTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Test\CustomGenerator;
+
+use Test\TestCase;
+
+/**
+ * Class CsvGeneratorTest
+ * Classe responsável por testar o gerador de CSV.
+ *
+ * @package Test\CustomGenerator
+ */
+class CsvGeneratorTest extends TestCase
+{
+    public function test()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,12 @@
+<?php
+namespace Test;
+/**
+ * Class TestCase
+ *
+ * @package Tests
+ * @author Gabriel Anhaia <anhaia.gabriel@gmail.com>
+ */
+class TestCase extends \PHPUnit\Framework\TestCase
+{
+
+}
